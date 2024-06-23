02f6b2016d11e1d5dc739187bbb5e900e089df08
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-23 22:28:51 +0530

    Done Basic UI Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,71 @@ import { fetchWeatherData } from './Helpers/fetchData.js';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [aqiValue, setAqiValue] = useState(true);
   const [cityName, setCityName] = useState('');
 
-  async function getWeatherData(){
-    try{
-      const weatherData = fetchWeatherData(cityName);
-      toast.promise(weatherData, {
-        loading : 'Fetching Weather Data for the requested Location',
-        success : (data) => {
-          console.log(data);
+  async function getWeatherData(e) {
+    e.preventDefault();
+    try {
+      const weatherDataPromise = fetchWeatherData(cityName, aqiValue);
+      toast.promise(weatherDataPromise, {
+        loading: 'Fetching Weather Data for the requested Location',
+        success: (data) => {
           setWeatherData(data);
           return `Successfully fetched Weather Data for ${cityName}`;
         },
-        error : `Failed to fetch weather data for the requested city`
-      })
-      const data = await weatherData;
+        error: 'Failed to fetch weather data for the requested city'
+      });
+      const data = await weatherDataPromise;
       setWeatherData(data);
-    }catch(err){
-      console.error(`Error fetching weather data : `, err);
+    } catch (err) {
+      console.error(`Error fetching weather data: `, err);
     }
   }
 
-
-  return(
+  return (
     <>
-        <h1 className="text-white text-center text-2xl bg-slate-700 p-4 m-2 font-mono">Weather-App-React</h1>
-        <label htmlFor="">City Name : </label>
-        <input
-          type="text"
-          className=" border-solid border-2 border-slate-400"
-          value={cityName}
-          onChange={(e) => setCityName(e.target.value)}
-        />
+      <h1 className="text-white text-center text-2xl bg-slate-700 p-4 m-2 font-mono">Weather-App-React</h1>
+      <form action="" className="flex justify-center items-center flex-col" onSubmit={getWeatherData}>
+        <div>
+          <label htmlFor="city">City Name: </label>
+          <input
+            type="text"
+            id="city"
+            className="border-solid border-2 border-slate-400"
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+          />
+        </div>
+        <div className="flex gap-2">
+          <label htmlFor="aqi">AQI</label>
+          <input
+            type="checkbox"
+            id="aqi"
+            checked={aqiValue}
+            onChange={(e) => setAqiValue(e.target.checked)}
+          />
+        </div>
         <br />
-        <button onClick={getWeatherData} className="text-md bg-slate-400 p-4 m-4 rounded-lg border-solid border-4 border-gray-500 text-white hover:text-slate-700 hover:bg-gray-300">
+        <button type="submit" className="text-md bg-slate-400 p-4 m-4 rounded-lg border-solid border-4 border-gray-500 text-white hover:text-slate-700 hover:bg-gray-300">
           Show Weather Data
         </button>
-        <Toaster/>
-        {
-          weatherData && (
-            <div>
-              <h2>Temperature in Celsius : {weatherData.current.temp_c}</h2>
-              <h2>Humidity : {weatherData.humidity}</h2>
-              <h2>Time : {weatherData.location.localtime}</h2>
-            </div>
-          )
-        }
+        <Toaster />
+      </form>
+      {weatherData && (
+        <div>
+          <h2>Temperature in Celsius: {weatherData?.current?.temp_c}</h2>
+          <h2>Humidity: {weatherData?.current?.humidity}</h2>
+          <h2>Time: {(weatherData?.location?.localtime).split(" ")[1]}</h2>
+          <h2>Weather: {weatherData?.current?.condition?.text} <img src={weatherData?.current?.condition?.icon} alt="" /></h2>
+          {
+            aqiValue &&
+            (<h2>AQI : {weatherData?.current?.air_quality?.co}</h2>)
+          }
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Helpers/fetchData.js
+++ src/Helpers/fetchData.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 
-async function fetchWeatherData(cityName){
+async function fetchWeatherData(cityName, aqiValue){
     const api_key = import.meta.env.VITE_API_KEY;
-    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${cityName}&aqi=yes`);
+    
+    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${cityName}&aqi=${aqiValue ? 'yes' : 'no' }`);
     console.log(response.data);
     return response.data;
 }
