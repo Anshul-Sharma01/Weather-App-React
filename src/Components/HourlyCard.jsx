20dea11557fddf0c0420fd7216b175edeeb6abf2
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-07-27 16:10:01 +0530

    done with react-router, left with ui

--- src/Components/HourlyCard.jsx
+++ src/Components/HourlyCard.jsx
@@ -1,11 +1,10 @@
-import { CiCloudOn } from "react-icons/ci";
-import { FaRegSnowflake, FaWind, FaCloudRain } from "react-icons/fa";
-import { MdAvTimer } from "react-icons/md";
+
 import { useParams } from "react-router-dom";
 import { fetchHourlyData } from "../Helpers/fetchData";
 import { useEffect, useState } from "react";
 
 import Loader from "./Loader";
+import HoursCard from "./HoursCard";
 
 function HourlyCard() {
     const [weatherData, setWeatherData] = useState(null);
@@ -37,28 +36,16 @@ function HourlyCard() {
     if (!weatherData || weatherData.error) {
         return <div>{weatherData?.error || 'No data available'}</div>;
     }
-
+ 
     return (
         <>
-            {/* <div className="flex flex-col items-center p-8 rounded-md w-60 sm:px-12 dark:bg-gray-50 dark:text-gray-800 shadow-2xl">
-                <div className="text-center">
-                    <MdAvTimer className="inline" />
-                    <p className="text-sm text-red-600">{weatherData?.time.split(" ")[1]}</p>
-                    <p>{weatherData?.time?.split(" ")[0].split("-")[1]}/{weatherData?.time?.split(" ")[0].split("-")[2]}</p>
-                </div>
-                <img src={`https:${weatherData?.condition?.icon}`} alt="Weather pic" />
-                <div className="mb-2 text-3xl font-semibold">{weatherData?.temp_c}°
-                    <span className="mx-1 font-normal">/</span>{weatherData?.temp_f}F
-                </div>
-                <div className="text-justify flex flex-col">
-                    <p className="dark:text-gray-600">{weatherData?.condition?.text}</p>
-                    <p>Humidity : {weatherData?.humidity}</p>
-                    <p><FaCloudRain className="inline" /> :{weatherData?.chance_of_rain}%</p>
-                    <p><FaRegSnowflake className="inline" /> : {weatherData?.chance_of_snow}%</p>
-                    <p><CiCloudOn className="inline" /> : {weatherData?.cloud}%</p>
-                    <p><FaWind className="inline" /> : {weatherData?.wind_kph} kph</p>
-                </div>
-            </div> */}
+            <section className="flex flex-row items-center justify-center gap-10 flex-wrap">
+                {
+                    weatherData?.hour.map((ele) => (
+                        <HoursCard weatherData={ele} key={ele.time_epoch}/>
+                    ))
+                }
+            </section>
         </>
     );
 }
---
+++ src/Components/HoursCard.jsx
@@ -0,0 +1,34 @@
+import { CiCloudOn } from "react-icons/ci";
+import { FaRegSnowflake, FaWind, FaCloudRain } from "react-icons/fa";
+import { MdAvTimer } from "react-icons/md";
+
+function HoursCard({ weatherData }){
+    return(
+        <>
+            <div className="flex flex-col items-center p-8 rounded-md w-60 sm:px-12 dark:bg-gray-50 dark:text-gray-800 shadow-2xl">
+                    <div className="text-center">
+                        <MdAvTimer className="inline" />
+                        <p className="text-sm text-red-600">{weatherData?.time.split(" ")[1]}</p>
+                        <p>{weatherData?.time?.split(" ")[0].split("-")[1]}/{weatherData?.time?.split(" ")[0].split("-")[2]}</p>
+                    </div>
+                    <img src={`https:${weatherData?.condition?.icon}`} alt="Weather pic" />
+                    <div className="mb-2 text-3xl font-semibold">{weatherData?.temp_c}°
+                        <span className="mx-1 font-normal">/</span>{weatherData?.temp_f}F
+                    </div>
+                    <div className="text-justify flex flex-col">
+                        <p className="dark:text-gray-600">{weatherData?.condition?.text}</p>
+                        <p>Humidity : {weatherData?.humidity}</p>
+                        <p><FaCloudRain className="inline" /> :{weatherData?.chance_of_rain}%</p>
+                        <p><FaRegSnowflake className="inline" /> : {weatherData?.chance_of_snow}%</p>
+                        <p><CiCloudOn className="inline" /> : {weatherData?.cloud}%</p>
+                        <p><FaWind className="inline" /> : {weatherData?.wind_kph} kph</p>
+                    </div>
+                </div>
+        </>
+    )
+}
+
+
+export default HoursCard;
+
+
